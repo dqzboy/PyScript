90a1ec54f9d319912c9c12ce6a261e5e44fd48f0
dqzboy <42825450+dqzboy@users.noreply.github.com>
2022-08-29 13:29:08 +0800

    Add files via upload

---
+++ wechat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+import json
+import sys
+import os
+
+headers = {'Content-Type': 'application/json;charset=utf-8'}
+api_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c4f1290a-xxx"
+
+def msg(text):
+    json_text= {
+     "msgtype": "text",
+        "text": {
+            "content": text
+        },
+    }
+    print requests.post(api_url,json.dumps(json_text),headers=headers).content
+
+if __name__ == '__main__':
+    text = sys.argv[1]
+    msg(text)
